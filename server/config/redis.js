809372b2ecbf210800f9c4d1480741a8d3359587
PradhumGupta <guptapradhum77@gmail.com>
2025-11-28 18:46:46 +0530

    env updated

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,12 +1,12 @@
 import { createClient } from 'redis';
-import { REDIS_HOST, REDIS_PASSWORD } from './env.js';
+import { REDIS_HOST, REDIS_PASSWORD, REDIS_PORT } from './env.js';
 
 const redisClient = createClient({
     username: 'default',
     password: REDIS_PASSWORD,
     socket: {
         host: REDIS_HOST,
-        port: 12558
+        port: REDIS_PORT
     }
 });
 
